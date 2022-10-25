40d217057c16cf49e1af40ffdafd6a573b683bfa
Konstantin Pukach <pukachkosnt@gmail.com>
2022-10-25 12:59:57 +0300

    Android [CI]: Test CI with cache 3

--- baby-eye-android/features/login/ui/src/main/kotlin/com/pukachkosnt/babyeye/features/login/ui/LoginScreen.kt
+++ baby-eye-android/features/login/ui/src/main/kotlin/com/pukachkosnt/babyeye/features/login/ui/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.pukachkosnt.babyeye.features.login.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -10,7 +11,21 @@ internal fun LoginScreen(
     loginViewModel: LoginViewModel,
     navController: NavController
 ) {
+    Column {
         Button(onClick = { /*TODO*/ }) {
             Text(text = "Button")
         }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Button2")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Button3")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Button2")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Button3")
+        }
+    }
 }
